dec1ce3d852fdc6857e97b6e9603432f3ca50ffb
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2024-12-04 14:47:22 +1100

    feat(item): move all items to vanilla item groups

--- src/main/java/dev/spiritstudios/hollow/HollowItemGroupAdditions.java
+++ src/main/java/dev/spiritstudios/hollow/HollowItemGroupAdditions.java
@@ -1,15 +1,20 @@
 package dev.spiritstudios.hollow;
 
 import dev.spiritstudios.hollow.block.HollowLogBlock;
+import dev.spiritstudios.hollow.component.CopperInstrument;
 import dev.spiritstudios.hollow.registry.HollowBlocks;
+import dev.spiritstudios.hollow.registry.HollowDataComponentTypes;
+import dev.spiritstudios.hollow.registry.HollowItems;
 import dev.spiritstudios.specter.api.core.util.ReflectionHelper;
 import it.unimi.dsi.fastutil.objects.Object2ReferenceOpenHashMap;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.Blocks;
-import net.minecraft.item.Item;
-import net.minecraft.item.Items;
+import net.minecraft.component.ComponentChanges;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 
+import java.util.Arrays;
 import java.util.Map;
 
 public final class HollowItemGroupAdditions {
@@ -18,18 +23,66 @@ public final class HollowItemGroupAdditions {
         ReflectionHelper.forEachStaticField(HollowBlocks.class, HollowLogBlock.class, (block, name, field) -> baseToHollow.put(Registries.ITEM.get(block.typeData.id()), block));
 
         ItemGroupEvents.MODIFY_ENTRIES_ALL.register((itemGroup, entries) -> {
-            baseToHollow.forEach((item, block) -> {
-                if (itemGroup.contains(item.getDefaultStack())) entries.addAfter(item, block);
-            });
+            ItemGroupHelper helper = new ItemGroupHelper(itemGroup, entries);
 
-            if (itemGroup.contains(Items.PINK_PETALS.getDefaultStack())) {
-                entries.addAfter(
-                        Items.PINK_PETALS.getDefaultStack(),
-                        HollowBlocks.PURPLE_WILDFLOWER,
-                        HollowBlocks.BLUE_WILDFLOWER,
-                        HollowBlocks.WHITE_WILDFLOWER
-                );
-            }
+            baseToHollow.forEach(helper::addAfter);
+
+            helper.addAfter(
+                    Items.PINK_PETALS,
+                    HollowBlocks.PURPLE_WILDFLOWER,
+                    HollowBlocks.BLUE_WILDFLOWER,
+                    HollowBlocks.WHITE_WILDFLOWER
+            );
+            helper.addAfter(Items.ALLIUM, HollowBlocks.PAEONIA);
+            helper.addAfter(Items.BLUE_ORCHID, HollowBlocks.ROOTED_ORCHID);
+            helper.addAfter(Items.PEONY, HollowBlocks.CAMPION);
+            helper.addAfter(Items.DEAD_BUSH, HollowBlocks.TWIG);
+            helper.addAfter(Items.KELP, HollowBlocks.CATTAIL);
+            helper.addAfter(Items.LILY_PAD, HollowItems.LOTUS_LILYPAD, HollowItems.GIANT_LILYPAD);
+            helper.addAfter(Items.BROWN_MUSHROOM, HollowBlocks.POLYPORE);
+
+            helper.addAfter(Items.DECORATED_POT, HollowBlocks.JAR, HollowBlocks.FIREFLY_JAR);
+
+            helper.addAfter(Items.SCULK_CATALYST, HollowBlocks.SCULK_JAW);
+            helper.addAfter(Items.VAULT, HollowBlocks.ECHOING_POT);
+            helper.addAfter(Items.CHEST, HollowBlocks.STONE_CHEST, HollowBlocks.STONE_CHEST_LID);
+
+            helper.addAfter(Items.CHISELED_COPPER, HollowBlocks.COPPER_PILLAR);
+            helper.addAfter(Items.EXPOSED_CHISELED_COPPER, HollowBlocks.EXPOSED_COPPER_PILLAR);
+            helper.addAfter(Items.WEATHERED_CHISELED_COPPER, HollowBlocks.WEATHERED_COPPER_PILLAR);
+            helper.addAfter(Items.OXIDIZED_CHISELED_COPPER, HollowBlocks.OXIDIZED_COPPER_PILLAR);
+
+            helper.addAfter(Items.WAXED_CHISELED_COPPER, HollowBlocks.WAXED_COPPER_PILLAR);
+            helper.addAfter(Items.WAXED_EXPOSED_CHISELED_COPPER, HollowBlocks.WAXED_EXPOSED_COPPER_PILLAR);
+            helper.addAfter(Items.WAXED_WEATHERED_CHISELED_COPPER, HollowBlocks.WAXED_WEATHERED_COPPER_PILLAR);
+            helper.addAfter(Items.WAXED_OXIDIZED_CHISELED_COPPER, HollowBlocks.WAXED_OXIDIZED_COPPER_PILLAR);
         });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(entries -> {
+            entries.add(HollowItems.FIREFLY_SPAWN_EGG);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
+            entries.add(HollowItems.MUSIC_DISC_POSTMORTEM);
+
+
+            entries.addAfter(
+                    Items.GOAT_HORN,
+                    Arrays.stream(CopperInstrument.values()).map(instrument -> new ItemStack(
+                            Registries.ITEM.getEntry(HollowItems.COPPER_HORN),
+                            1,
+                            ComponentChanges.builder()
+                                    .add(HollowDataComponentTypes.COPPER_INSTRUMENT, instrument)
+                                    .build()
+                    )).toArray(ItemStack[]::new)
+            );
+        });
+    }
+
+    private record ItemGroupHelper(ItemGroup group, FabricItemGroupEntries entries) {
+        public void addAfter(Item after, ItemConvertible... add) {
+            if (group.contains(after.getDefaultStack()))
+                entries.addAfter(after, add);
+        }
     }
 }
---
+++ src/main/java/dev/spiritstudios/hollow/mixin/SculkVeinGrowCheckerMixin.java
@@ -0,0 +1,23 @@
+package dev.spiritstudios.hollow.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import dev.spiritstudios.hollow.registry.HollowBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(targets = "net.minecraft.block.SculkVeinBlock.SculkVeinGrowChecker")
+public abstract class SculkVeinGrowCheckerMixin {
+    @WrapMethod(
+            method = "canGrow(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/Direction;Lnet/minecraft/block/BlockState;)Z"
+    )
+    private boolean canGrow(BlockView world, BlockPos pos, BlockPos growPos, Direction direction, BlockState state, Operation<Boolean> original) {
+        return original.call(world, pos, growPos, direction, state) && !world.getBlockState(pos.down()).isOf(HollowBlocks.SCULK_JAW);
+    }
+}
