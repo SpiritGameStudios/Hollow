9f280200c773f409721f5bb606a8db2086b161d7
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2025-01-20 20:28:40 +1100

    chore: datagen damagetypes and their tags

---
+++ src/main/java/dev/spiritstudios/hollow/datagen/DamageTypeProvider.java
@@ -0,0 +1,32 @@
+package dev.spiritstudios.hollow.datagen;
+
+import dev.spiritstudios.hollow.Hollow;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
+import net.minecraft.entity.damage.DamageType;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class DamageTypeProvider extends FabricDynamicRegistryProvider {
+    public DamageTypeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup, Entries entries) {
+        RegistryWrapper<DamageType> lookup = wrapperLookup.getOrThrow(RegistryKeys.DAMAGE_TYPE);
+
+        lookup.streamKeys()
+                .filter(key ->
+                        key.getValue().getNamespace().equals(Hollow.MODID))
+                .forEach(key ->
+                        entries.add(key, lookup.getOrThrow(key).value()));
+    }
+
+    @Override
+    public String getName() {
+        return "Hollow/Damage Types";
+    }
+}
