1dfa741c0f9bdf8ed4f81dfe528e38bed78a40f9
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2025-07-30 17:30:12 +1000

    tweak(worldgen): add red mushrooms to swamp bedrock parity

--- src/main/java/dev/spiritstudios/hollow/worldgen/HollowBiomeModifications.java
+++ src/main/java/dev/spiritstudios/hollow/worldgen/HollowBiomeModifications.java
@@ -16,10 +16,11 @@ import net.minecraft.entity.SpawnRestriction;
 import net.minecraft.sound.MusicType;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.biome.BiomeKeys;
-import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.feature.VegetationPlacedFeatures;
 
 import java.util.function.Predicate;
+
+import static net.minecraft.world.gen.GenerationStep.Feature.VEGETAL_DECORATION;
 
 public class HollowBiomeModifications {
     public static void init() {
@@ -43,41 +44,43 @@ public class HollowBiomeModifications {
 
         SpawnRestriction.register(HollowEntityTypes.FIREFLY, SpawnRestriction.getLocation(HollowEntityTypes.FIREFLY), Heightmap.Type.WORLD_SURFACE, FireflyEntity::canSpawn);
 
-        BiomeModifications.create(Hollow.id("hollow_swamp")).add(ModificationPhase.ADDITIONS, BiomeSelectors.includeByKey(BiomeKeys.SWAMP), context -> {
-            BiomeModificationContext.GenerationSettingsContext generationSettings = context.getGenerationSettings();
+        BiomeModifications.create(Hollow.id("swamp")).add(ModificationPhase.ADDITIONS, BiomeSelectors.includeByKey(BiomeKeys.SWAMP), context -> {
+            BiomeModificationContext.GenerationSettingsContext settings = context.getGenerationSettings();
 
-            generationSettings.addFeature(GenerationStep.Feature.VEGETAL_DECORATION, HollowPlacedFeatures.HUGE_BROWN_MUSHROOM_SWAMP);
+            settings.addFeature(VEGETAL_DECORATION, HollowPlacedFeatures.HUGE_BROWN_MUSHROOM_SWAMP);
+            settings.addFeature(VEGETAL_DECORATION, HollowPlacedFeatures.HUGE_RED_MUSHROOM_SWAMP);
         });
 
-        BiomeModifications.create(Hollow.id("hollow_swamps")).add(ModificationPhase.ADDITIONS, swamps, context -> {
-            BiomeModificationContext.GenerationSettingsContext generationSettings = context.getGenerationSettings();
+        BiomeModifications.create(Hollow.id("swamps")).add(ModificationPhase.ADDITIONS, swamps, context -> {
+            BiomeModificationContext.GenerationSettingsContext settings = context.getGenerationSettings();
 
-            generationSettings.addFeature(GenerationStep.Feature.VEGETAL_DECORATION, HollowPlacedFeatures.PATCH_TWIG);
-            generationSettings.addFeature(GenerationStep.Feature.VEGETAL_DECORATION, HollowPlacedFeatures.PATCH_GIANT_LILYPAD);
-            generationSettings.addFeature(GenerationStep.Feature.VEGETAL_DECORATION, HollowPlacedFeatures.CATTAILS);
+            settings.addFeature(VEGETAL_DECORATION, HollowPlacedFeatures.PATCH_TWIG);
+            settings.addFeature(VEGETAL_DECORATION, HollowPlacedFeatures.PATCH_GIANT_LILYPAD);
+            settings.addFeature(VEGETAL_DECORATION, HollowPlacedFeatures.CATTAILS);
         });
 
         BiomeModifications.addFeature(
                 BiomeSelectors.includeByKey(BiomeKeys.FOREST, BiomeKeys.FLOWER_FOREST, BiomeKeys.SWAMP),
-                GenerationStep.Feature.VEGETAL_DECORATION,
+                VEGETAL_DECORATION,
                 HollowPlacedFeatures.FALLEN_OAK
         );
 
-        BiomeModifications.create(Hollow.id("hollow_birch")).add(ModificationPhase.ADDITIONS, birch, context -> {
-            BiomeModificationContext.GenerationSettingsContext generationSettings = context.getGenerationSettings();
+        BiomeModifications.create(Hollow.id("birch")).add(ModificationPhase.ADDITIONS, birch, context -> {
+            BiomeModificationContext.GenerationSettingsContext settings = context.getGenerationSettings();
 
-            if (HollowConfig.INSTANCE.music.get())
-                context.getEffects().setMusic(MusicType.createIngameMusic(HollowSoundEvents.MUSIC_OVERWORLD_BIRCH_FOREST));
+            if (HollowConfig.INSTANCE.music.get()) {
+				context.getEffects().setMusic(MusicType.createIngameMusic(HollowSoundEvents.MUSIC_OVERWORLD_BIRCH_FOREST));
+			}
 
-            generationSettings.addFeature(GenerationStep.Feature.VEGETAL_DECORATION, HollowPlacedFeatures.PATCH_TWIG);
-            generationSettings.addFeature(GenerationStep.Feature.VEGETAL_DECORATION, HollowPlacedFeatures.PATCH_TALL_GRASS_BIRCH);
-            generationSettings.addFeature(GenerationStep.Feature.VEGETAL_DECORATION, HollowPlacedFeatures.PATCH_CAMPION);
-            generationSettings.addFeature(GenerationStep.Feature.VEGETAL_DECORATION, HollowPlacedFeatures.FALLEN_BIRCH);
+            settings.addFeature(VEGETAL_DECORATION, HollowPlacedFeatures.PATCH_TWIG);
+            settings.addFeature(VEGETAL_DECORATION, HollowPlacedFeatures.PATCH_TALL_GRASS_BIRCH);
+            settings.addFeature(VEGETAL_DECORATION, HollowPlacedFeatures.PATCH_CAMPION);
+            settings.addFeature(VEGETAL_DECORATION, HollowPlacedFeatures.FALLEN_BIRCH);
         }).add(ModificationPhase.REPLACEMENTS, birch, context -> {
-            BiomeModificationContext.GenerationSettingsContext generationSettings = context.getGenerationSettings();
+            BiomeModificationContext.GenerationSettingsContext settings = context.getGenerationSettings();
 
-            if (generationSettings.removeFeature(VegetationPlacedFeatures.PATCH_GRASS_FOREST))
-                generationSettings.addFeature(GenerationStep.Feature.VEGETAL_DECORATION, HollowPlacedFeatures.PATCH_GRASS_BIRCH);
+            if (settings.removeFeature(VegetationPlacedFeatures.PATCH_GRASS_FOREST))
+                settings.addFeature(VEGETAL_DECORATION, HollowPlacedFeatures.PATCH_GRASS_BIRCH);
         }).add(ModificationPhase.REMOVALS, birch, context ->
                 context.getGenerationSettings().removeFeature(VegetationPlacedFeatures.PATCH_PUMPKIN));
     }
