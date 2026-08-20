37f7d568c9918c9ef6bfcb31a6c9f8a7128a0df9
Axia <mcvoidredstone@gmail.com>
2026-08-20 11:39:02 +0100

    commit test

--- src/main/java/dev/spiritstudios/hollow/Hollow.java
+++ src/main/java/dev/spiritstudios/hollow/Hollow.java
@@ -26,6 +26,7 @@ public final class Hollow implements ModInitializer {
 
     @Override
     public void onInitialize() {
+		// axia was here
         HollowRegistryKeys.init();
         HollowSoundEvents.init();
 
