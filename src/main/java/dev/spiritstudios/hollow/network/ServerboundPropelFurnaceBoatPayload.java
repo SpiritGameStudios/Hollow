918390ac630bd738383a0e2bf78cfb4661734f81
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2026-09-04 13:05:26 +1000

    Clean up `ServerboundPropelFurnaceBoatPayload` streamcodec

--- src/main/java/dev/spiritstudios/hollow/network/ServerboundPropelFurnaceBoatPayload.java
+++ src/main/java/dev/spiritstudios/hollow/network/ServerboundPropelFurnaceBoatPayload.java
@@ -3,28 +3,26 @@ package dev.spiritstudios.hollow.network;
 import dev.spiritstudios.hollow.Hollow;
 import dev.spiritstudios.hollow.world.entity.vehicle.AbstractFurnaceBoat;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.core.UUIDUtil;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 
 import java.util.UUID;
 
 public record ServerboundPropelFurnaceBoatPayload(UUID furnaceBoat, boolean propelled) implements CustomPacketPayload {
-	public static final StreamCodec<FriendlyByteBuf, ServerboundPropelFurnaceBoatPayload> CODEC = StreamCodec.of(ServerboundPropelFurnaceBoatPayload::write, ServerboundPropelFurnaceBoatPayload::new);
+	public static final StreamCodec<FriendlyByteBuf, ServerboundPropelFurnaceBoatPayload> CODEC = StreamCodec.composite(
+		UUIDUtil.STREAM_CODEC, ServerboundPropelFurnaceBoatPayload::furnaceBoat,
+		ByteBufCodecs.BOOL, ServerboundPropelFurnaceBoatPayload::propelled,
+		ServerboundPropelFurnaceBoatPayload::new
+	);
+
 	public static final Type<ServerboundPropelFurnaceBoatPayload> TYPE = new Type<>(Hollow.id("serverbound_propel_furnace_boat_v1"));
 
 	@Override
 	public Type<? extends CustomPacketPayload> type() {
 		return TYPE;
-	}
-
-	public ServerboundPropelFurnaceBoatPayload(FriendlyByteBuf buf) {
-		this(buf.readUUID(), buf.readBoolean());
-	}
-
-	private static void write(FriendlyByteBuf buf, ServerboundPropelFurnaceBoatPayload payload) {
-		buf.writeUUID(payload.furnaceBoat);
-		buf.writeBoolean(payload.propelled);
 	}
 
 	public static class Receiver implements ServerPlayNetworking.PlayPayloadHandler<ServerboundPropelFurnaceBoatPayload> {
